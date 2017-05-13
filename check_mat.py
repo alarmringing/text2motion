138dcb19e0ef81f288dc4043ddda764da06f05c7
Jihee Hwang <jiheeh@stanford.edu>
2017-05-13 13:21:19 -0700

    Prunes by framerate as well, so that all input data has the same amount of frames

--- check_mat.py
+++ check_mat.py
@@ -2,7 +2,7 @@ import scipy.io as sio
 import collections
 import os
 
-def save_actions():
+def save_actions(data_dir):
 	'''
 	crawls through the data and gathers info into a big dictionary, full_action_dict
 
@@ -10,13 +10,12 @@ def save_actions():
 	key: action type
 	value: list of mat files
 
-	actions_oriented: 
+	actions_pruned: 
 	key: action type
-	value: indices of mat files that follow the most popular viewpoint within this action
+	value: indices of mat files that follow the most popular viewpoint and frame# within this action
 	'''
 	full_action_dict = collections.defaultdict(list)
-	actions_oriented = collections.defaultdict(list)
-	data_dir = 'joint_positions'
+	actions_pruned = collections.defaultdict(list)
 	actions = []
 
 	for action in os.listdir(data_dir):
@@ -26,6 +25,7 @@ def save_actions():
 	for action in actions: 
 		actiondir = data_dir + "/" + action
 		orientations = []
+		frames = []
 
 		#walks through each mat and saves them to full_action_dict
 		for folder in os.listdir(actiondir): 
@@ -33,11 +33,19 @@ def save_actions():
 				continue 
 			joints = sio.loadmat(actiondir + '/' + folder + '/joint_positions.mat')
 			full_action_dict[action].append(joints)
+			#save orientation and frame info
 			orientations.append(joints['viewpoint'][0])
+			frames.append(len(joints['scale'][0]))
 
-		#find what's the most common viewpoint for this action class, and saves indices for 
-		#mats with that orientation into actions_oriented
+		#find what's the most common viewpoint & frame for this action class, and saves indices for 
+		# mats with that orientation and frame # into actions_pruned
 		max_viewpoint = max(set(orientations), key=orientations.count)
-		actions_oriented[action] = [i for i,v in enumerate(full_action_dict[action]) if v['viewpoint'][0] == max_viewpoint]
+		max_frames = max(set(frames), key=frames.count)
+		actions_pruned[action] = [i for i,v in enumerate(full_action_dict[action]) \
+		if v['viewpoint'][0] == max_viewpoint and len(v['scale'][0]) == max_frames ]
 
-	return full_action_dict, actions_oriented
+		print("actions_pruned look like ", actions_pruned)
+
+	return full_action_dict, actions_pruned
+
+save_actions('joint_positions')
