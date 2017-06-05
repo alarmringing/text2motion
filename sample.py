6b664ee422681a9084b97b6deeaa2ee81169d272
Jihee Hwang <jiheeh@stanford.edu>
2017-06-04 17:42:25 -0700

    Now prints error every iteration

--- sample.py
+++ sample.py
@@ -5,6 +5,9 @@ import animation
 
 def sample(model_path, initial_input):
 	
+	def mean_squared_error(pred, truth):
+		return np.mean((pred - truth)**2)/(pred.size)
+
 	with tf.Session() as sess:
 		#restores the model 
 		saver = tf.train.import_meta_graph(model_path + 'model_lstm.meta')
@@ -16,19 +19,20 @@ def sample(model_path, initial_input):
 		pred_result = np.zeros((2, 15, T-1)) #this is fixed
 
 		#at each timestep
-		initial_timestep = initial_input
+		initial_timestep = initial_input #full ground truth information
 		for t in range(initial_input.shape[2]-1):
 			values = {
 				"input_data:0": initial_timestep
 			}
 			predictions = sess.run([graph.get_tensor_by_name("regression/predictions:0")], values)
-			this_pred = predictions[0][0,0,:]
-			print("shape of this_pred is ", this_pred.shape)
-			print("shape of initial_input si ", initial_input.shape)
+
+			this_pred = predictions[0][0, 0, :]
 			pred_result[0, :, t] = this_pred[:15]
 			pred_result[1, :, t] = this_pred[15:]
 			initial_timestep = np.concatenate((np.reshape(this_pred, (1,-1,1)), np.zeros((1,30,T-1))), axis=2) #second value is meaningless
-	
+			
+			#report error with ground truth in this timestep
+			print("timestep: ", t, " error: ", mean_squared_error(initial_input[:,:,t+1].flatten(), predictions[0][t,0,:].flatten()))
 		return pred_result
 
 def generate_initial_input(data_dir, action_class):
@@ -37,7 +41,6 @@ def generate_initial_input(data_dir, action_class):
 	lucky_ind = np.random.choice(len(action_data), 1) #randomly choose one instance! 
 	print("lucky ind is ", lucky_ind)
 	#reshape to 1*30*T
-	print("original shape is ", action_data[lucky_ind]['pos_world'])
 	stacked = np.vstack((action_data[lucky_ind]['pos_world'][0], action_data[lucky_ind]['pos_world'][1]))
 	stacked = np.expand_dims(stacked, axis=0)
 	return stacked
