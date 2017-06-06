49f234325964e35564275866a4020d001d66caf3
danishabbir <Danish@DN51vtis.SUNet>
2017-06-05 18:34:31 -0700

    saving animation file with varying frame rate

--- animation.py
+++ animation.py
@@ -7,6 +7,8 @@ import check_mat
 def animate_action(mat, title):
 	'''
 	Takes an joint position matrix 2*15*framerate, and animates it on pyplot.
+
+	Saves animation into an .mp4 file named title
 	'''
 	print("animating " + title)
 	numframes = mat.shape[2]
@@ -18,6 +20,8 @@ def animate_action(mat, title):
 	scat = plt.scatter(mat[0,:,0], mat[1,:,0], s=100)
 	plt.autoscale(False)
 	lines = plt.plot([],[])
+
+	#get_tight_layout()
 
 	#updates the scatter dots -- not showing right now, there's a bug
 	def update_dots(i, mat):
@@ -33,23 +37,30 @@ def animate_action(mat, title):
 		for pair in connections:
 			ind = np.array(list(pair))
 			plt.plot(dots[0, ind], dots[1, ind])
+		plt.scatter(dots[0], dots[1], s=30, alpha=1)
 
 	#animate fig
 	scatAnim = animation.FuncAnimation(fig, update_dots, blit=False, frames=xrange(numframes), interval = 1, fargs=(mat,))
 	plt.show()
+	filename = title+'.mp4'
+	scatAnim.save(filename, fps=15, extra_args=['-vcodec', 'libx264'])
 
 def animate_two(mat1, mat2):
 	
 	plt.subplot(1,2,1)
 	animate_action(mat1, "mat1")
-	plt.subplot(1,2,2)
+	#=plt.subplot(1,2,2)
 	animate_action(mat2, "mat2")
 
 #debugging
 if __name__ == '__main__':
 	full_action_dict, actions_pruned = check_mat.save_actions('data/joint_positions')
 	option1 = full_action_dict['shoot_bow'][actions_pruned['shoot_bow'][5]]['pos_world']
-	option2 = full_action_dict['shoot_bow'][actions_pruned['shoot_bow'][2]]['pos_world']
+	#option2 = full_action_dict['shoot_bow'][actions_pruned['shoot_bow'][2]]['pos_world']
+	
+	#animate_two(option1, option2)
 
-	animate_two(option1, option2)
+	animate_action(option1, "option1")
+
+
 
