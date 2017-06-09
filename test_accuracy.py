807b9e899f9ec8fffd0375ab17efa3a543d5a847
Jihee Hwang <alarmringing@gmail.com>
2017-06-09 08:11:13 +0000

    Now takes prune parameter

--- test_accuracy.py
+++ test_accuracy.py
@@ -4,12 +4,12 @@ import sample
 import sys
 import animation
 
-def report_test_error(savefile_name, action_class, test_inds, pre_step):
+def report_test_error(savefile_name, action_class, test_inds, pre_step, prune_viewpoints):
 	summ = 0
 	for ind in test_inds:
 		ind = int(ind)
 		print("Checking error for test_ind ", ind)
-		action = sample.extract_action('data/joint_positions', action_class, ind)
+		action = sample.extract_action('data/joint_positions', action_class, ind, prune_viewpoints)
 		initial_input = sample.generate_initial_input(action)
 		pred_result = sample.sample('data/', initial_input, savefile_name, pre_step)
 		animation.animate_action(pred_result, action_class, ind,'Prediction of %s'%action_class)
@@ -22,5 +22,6 @@ if __name__ == '__main__':
 	action_class = sys.argv[2]
 	test_inds = sys.argv[3]
 	pre_step = 10
-	err = report_test_error(savefile_name, action_class, test_inds.split(','), pre_step)
+	prune_viewpoints = False
+	err = report_test_error(savefile_name, action_class, test_inds.split(','), pre_step, prune_viewpoints)
 	print("Average test error for " + action_class + " is ", err)
